fcdd32243d273af9b680085585d5a749103d715c
Bruno <brunokristian003@gmail.com>
2025-05-15 10:30:12 +0200

    mobile done

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,12 +1,22 @@
 <section class="about section" data-theme="light">
-    <div class="grid-container" style="height: 60vh;">
-        <div class="grid-item image" style="grid-column: 1 / 4; grid-row: 1 / 9;"><img src="{{ asset('images/gardener_3.jpg') }}" alt="flowers" /></div>
-        <div class="grid-item text" style="grid-column: 1/ 4; grid-row: 9 / 10;">zakladatel PETER</div>
-        <div class="grid-item text" style="grid-column: 4 / 10; grid-row: 1 / 2;">Sme na trhu už 30 rokov a  nebojíme sa žiadneho pojektu.</div>
-        <div class="grid-item image" style="grid-column: 4 / 7; grid-row: 2 / 10;"><img src="{{ asset('images/flowers.jpg') }}" alt="tomato" /></div>
-        <div class="grid-item text" style="grid-column: 7 / 10; grid-row: 2 / 7;"><h1>Tradičná rodinná firma z Malaciek.</h1></div>
-        <div class="grid-item text" style="grid-column: 10 / 13; grid-row: 9 / 10;">syn TOMÁŠ</div>
-        <div class="grid-item image" style="grid-column: 10 / 13; grid-row: 1 / 9;"><img src="{{ asset('images/gardener_2.jpg') }}" alt="hedge trimming" /></div>
-        <div class="grid-item image" style="grid-column: 7 / 10; grid-row: 7 / 10;"><img src="{{ asset('images/vegetables.jpg') }}" alt="hedge trimming" /></div>
+    <div class="grid-container">
+        <div class="grid-item image">
+            <img src="{{ asset('images/gardener_3.jpg') }}" alt="flowers" />
+        </div>
+        <div class="grid-item text">zakladateľ PETER</div>
+        <div class="grid-item text">Sme na trhu už 30 rokov a nebojíme sa žiadneho projektu.</div>
+        <div class="grid-item image">
+            <img src="{{ asset('images/flowers.jpg') }}" alt="tomato" />
+        </div>
+        <div class="grid-item text">
+            <h1>Tradičná rodinná firma z Malaciek.</h1>
+        </div>
+        <div class="grid-item text">syn TOMÁŠ</div>
+        <div class="grid-item image">
+            <img src="{{ asset('images/gardener_2.jpg') }}" alt="hedge trimming" />
+        </div>
+        <div class="grid-item image">
+            <img src="{{ asset('images/vegetables.jpg') }}" alt="hedge trimming" />
+        </div>
     </div>
 </section>
--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -2,7 +2,7 @@
     <h2>Naše nedávne projekty</h2>
     <div class="slider">
         <div class="card">
-            <h3>Záhrada s prvkami divokej prírody</h3>
+            <h3>Divoká záhrada</h3>
             <img src="{{ asset('images/flowers.jpg') }}" alt="flowers" />
             <div class="content">
                 <div class="token">Dunajská Lužná</div>
